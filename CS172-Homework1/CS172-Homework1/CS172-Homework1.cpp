34295adc544305871db05490eb54c243cc59d26c
cnickelson19 <cnickelson19@my.whitworth.edu>
2016-09-15 15:32:19 -0700

    Bare bones structure

---
+++ CS172-Homework1/CS172-Homework1/CS172-Homework1.cpp
@@ -0,0 +1,39 @@
+#include<iostream> 
+#include<cmath> 
+#include<string> 
+#include<ctime> 
+
+using namespace std;
+
+void ex02();
+void ex03();
+void ex04();
+void ex05();
+
+int main()
+{
+	ex02();
+	ex03();
+	ex04();
+	ex05();
+}
+
+void ex02()
+{
+
+}
+
+void ex03()
+{
+
+}
+
+void ex04()
+{
+
+}
+
+void ex05()
+{
+
+}
