65c50a5c384de4f164f55d49539754f4cf2290ad
cnickelson19 <cnickelson19@my.whitworth.edu>
2016-09-19 12:13:42 -0700

    ex 04 commented

--- CS172-Homework1/CS172-Homework1/CS172-Homework1.cpp
+++ CS172-Homework1/CS172-Homework1/CS172-Homework1.cpp
@@ -65,11 +65,13 @@ void ex03()
 {
 	cout << "What is the area of the square?" << endl;
 	double area;
+	// retrieve the area from the user
 	cin >> area;
 	// The diagonal is the square root of the sum of the squares of the sides. Because one side is the square root of the area, the diagonal is the square root of 2 times the area
 	cout << "The diagonal of this square is " << sqrt(2 * area) << "." << endl;
 	cout << "Yes or no (y or n)?" << endl;
 	char answer;
+	// get an answer from the user
 	cin >> answer;
 	// convert the letter to lowercase in case they entered a capital
 	answer = tolower(answer);
@@ -89,33 +91,42 @@ void ex04()
 	cout << "Enter an integer between 1 and 10." << endl;
 	int number;
 	cin >> number;
+	// as long as the user enters a number outside this range, the while loop will tell them to enter a new value
 	while (number < 1 || number>10)
 	{
 		cout << "Invalid input. Please try again." << endl;
 		cin >> number;
 	}
 	int sum = 0;
+	// use a for loop and the pow command with a temporary sum variable to amass the totals of the cubes up to the user's number from before
 	for (int i = 1; i <= number; i++)
 	{
 		sum+=pow(i, 3);
 	}
 	cout << "The sum of all of the cubes from 1 to " << number << " is " << sum << "." << endl;
 	int a = 0;
+	// have the program output an asterisk until the a variable (which is incremented each iteration) reaches the value of the user's number
 	do
 	{
 		cout << "* ";
 		a++;
 	} while (a < number);
 	cout <<endl<< "The even numbers from 0 to 40 are:" << endl;
+	// a for loop is used starting at 0 and ending at 40 where the index is increased by 2 each time and then dislpayed.
 	for (int i = 0; i <= 40; i += 2)
 		cout << i << " ";
+	// double the number with the function
 	doubling(number);
 	cout << endl<<"Twice the number you entered above is " << number << "." << endl;
+	// randomize two numbers to be added
 	int one = rand() % 100 + 1;
 	int two = rand() % 100 + 1;
 	cout << "a=" << one << ", b=" << two << "." << endl;
+	// display the total with the return value from the function
 	cout << "a+b=" << add(one, two) << endl;
+	// reset the user's number to its original value
 	number /= 2;
+	// increment it with the function
 	increment(number);
 	cout << "One more than the number you entered earlier is " << number << "." << endl;
 }
